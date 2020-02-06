4811fc54c00a537a3537293b6ed20aa4b63c5f59
ohara_tsunenori <tsunenori.ohara@drecom.co.jp>
2020-02-06 22:30:31 +0900

    use new syntax

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "taa" {
 }
 
 resource "aws_subnet" "taa1" {
-  vpc_id = "aws_vpc.taa.id"
+  vpc_id = aws_vpc.taa.id
   availability_zone = "ap-northeast-1a"
   cidr_block = "10.40.0.0/20"
   map_public_ip_on_launch = true
@@ -20,7 +20,7 @@ resource "aws_subnet" "taa1" {
 }
 
 resource "aws_subnet" "taa2" {
-  vpc_id = "aws_vpc.taa.id"
+  vpc_id = aws_vpc.taa.id
   availability_zone = "ap-northeast-1a"
   cidr_block = "10.40.16.0/20"
   map_public_ip_on_launch = true
@@ -31,7 +31,7 @@ resource "aws_subnet" "taa2" {
 }
 
 resource "aws_internet_gateway" "taa" {
-  vpc_id = "aws_vpc.taa.id"
+  vpc_id = aws_vpc.taa.id
   tags = {
     Name     = "taa-igw"
     Resource = "taa"
@@ -39,10 +39,10 @@ resource "aws_internet_gateway" "taa" {
 }
 
 resource "aws_route_table" "taa" {
-  vpc_id = "aws_vpc.taa.id"
+  vpc_id = aws_vpc.taa.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "aws_internet_gateway.taa.id"
+    gateway_id = aws_internet_gateway.taa.id
   }
   tags = {
     Name     = "taa-routing-table"
@@ -51,6 +51,6 @@ resource "aws_route_table" "taa" {
 }
 
 resource "aws_route_table_association" "taa1" {
-  subnet_id      = "aws_subnet.taa1.id"
-  route_table_id = "aws_route_table.taa.id"
+  subnet_id      = aws_subnet.taa1.id
+  route_table_id = aws_route_table.taa.id
 }
