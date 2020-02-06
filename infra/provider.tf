3199c432bd8925714ac1062bf418f19001da0827
ohara_tsunenori <tsunenori.ohara@drecom.co.jp>
2020-02-06 21:45:46 +0900

    add vpc tf

---
+++ infra/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  version = "2.47.0"
+  region = "ap-northeast-1"
+  shared_credentials_file = "credentials"
+}
---
+++ infra/vpc.tf
@@ -0,0 +1,56 @@
+resource "aws_vpc" "taa" {
+  cidr_block           = "10.40.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name     = "taa-vpc"
+    Resource = "taa"
+  }
+}
+
+resource "aws_subnet" "taa1" {
+  vpc_id = "aws_vpc.taa.id"
+  availability_zone = "ap-northeast-1a"
+  cidr_block = "10.40.0.0/20"
+  map_public_ip_on_launch = true
+  tags = {
+    Name     = "taa-subnet1"
+    Resource = "taa"
+  }
+}
+
+resource "aws_subnet" "taa2" {
+  vpc_id = "aws_vpc.taa.id"
+  availability_zone = "ap-northeast-1a"
+  cidr_block = "10.40.16.0/20"
+  map_public_ip_on_launch = true
+  tags = {
+    Name     = "taa-subnet2"
+    Resource = "taa"
+  }
+}
+
+resource "aws_internet_gateway" "taa" {
+  vpc_id = "aws_vpc.taa.id"
+  tags = {
+    Name     = "taa-igw"
+    Resource = "taa"
+  }
+}
+
+resource "aws_route_table" "taa" {
+  vpc_id = "aws_vpc.taa.id"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "aws_internet_gateway.taa.id"
+  }
+  tags = {
+    Name     = "taa-routing-table"
+    Resource = "taa"
+  }
+}
+
+resource "aws_route_table_association" "taa1" {
+  subnet_id      = "aws_subnet.taa1.id"
+  route_table_id = "aws_route_table.taa.id"
+}
