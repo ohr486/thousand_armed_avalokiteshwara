be9607630e25ded8ba935e329ff7dee8f383715a
ohr486 <ohr486@gmail.com>
2020-09-21 18:55:32 +0900

    add secureity groups

---
+++ infra/sg.tf
@@ -0,0 +1,73 @@
+resource "aws_security_group" "ping" {
+  vpc_id      = aws_vpc.taa.id
+  name        = "Ping SG"
+  description = "Ping Only"
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [var.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ssh" {
+  vpc_id      = aws_vpc.taa.id
+  name        = "SSH SG"
+  description = "SSH SG"
+
+  ingress {
+    from_port = -1
+    to_port   = -1
+    protocol  = "icmp"
+    cidr_blocks = [var.cidr_block]
+  }
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = [var.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "web" {
+  vpc_id      = aws_vpc.taa.id
+  name        = "Web SG"
+  description = "Web SG"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = [var.cidr_block]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = [var.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
